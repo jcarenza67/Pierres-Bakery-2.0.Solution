645a461b1bd1804f35fe5b1d89e792b5f8229db5
Joseph Wilfong <josephwilfong91@gmail.com>
2023-05-12 13:26:40 -0700

    resolve routing issue

--- PierresBakery/Controllers/VendorsController.cs
+++ PierresBakery/Controllers/VendorsController.cs
@@ -21,9 +21,9 @@ namespace PierresBakery.Controllers
     }
 
     [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName)
+    public ActionResult Create(string vendorName, string description)
     {
-      Vendor newVendor = new Vendor(vendorName);
+      Vendor newVendor = new Vendor(vendorName, description);
       return RedirectToAction("Index");
     }
 
