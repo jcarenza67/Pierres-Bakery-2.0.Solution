de980b1a062255574bf4de04efadd0a9039ca8fe
Joseph Wilfong <josephwilfong91@gmail.com>
2023-05-12 10:39:07 -0700

    update all cshtml still a WIP

--- PierresBakery/Controllers/VendorsController.cs
+++ PierresBakery/Controllers/VendorsController.cs
@@ -1,5 +1,7 @@
 using PierresBakery.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
 
 namespace PierresBakery.Controllers
 {
@@ -8,7 +10,28 @@ namespace PierresBakery.Controllers
     [HttpGet("/vendors")]
     public ActionResult Index()
     {
+      List<Vendor> allVendors = Vendor.GetAll();
       return View();
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Vendor foundVendor = Vendor.Find(id);
+      return View(foundVendor);
     }
   }
 }
