3107cd793b146d046d3090d31a636edb62cc13cf
Joseph Wilfong <122309417+jcarenza67@users.noreply.github.com>
2023-05-12 08:05:17 -0700

    Initial commit

---
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+    public class ClassNameTests
+    {
+      [TestMethod]
+      public void MethodName_DescriptionOfBehavior_ExpectedReturnValue()
+      {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+      }
+    }
+}
